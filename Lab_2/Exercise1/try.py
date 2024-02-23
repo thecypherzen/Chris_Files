318f19c5a236f06f4395c34eacdc6e43ce3409b7
William Inyam <sw.inyam@outlook.com>
2024-02-23 10:32:09 +0000

    refactor(L2:E2): resize bar size

--- Lab_2/Exercise1/try.py
+++ Lab_2/Exercise1/try.py
@@ -10,7 +10,7 @@ word_freq = freqDist[word]
 x = [1]
 y = [word_freq]
 print(x, y)
-plt.bar(x, y, color="#2AAA8A", width=0.5)
+plt.bar(x, y, color="#2AAA8A", width=1)
 plt.xlabel(word)
 plt.ylabel("Frequency")
 plt.savefig("text1_plot_this")
